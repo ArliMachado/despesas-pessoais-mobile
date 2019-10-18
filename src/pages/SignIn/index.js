97c17a7df82c534e6d5ef126dcb2d7aea5814843
Arli <arli.machado@gmail.com>
2019-10-18 17:50:06 -0300

    Definindo tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
+import { Alert } from 'react-native';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    Alert.alert(email);
+  }
+
   return (
     <Background>
-      <Text>Login</Text>
+      <Container>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Digite de email"
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            returnKeyType="send"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+        </Form>
+        <SignLink
+          onPress={() => {
+            navigation.navigate('SignUp');
+          }}
+        >
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
