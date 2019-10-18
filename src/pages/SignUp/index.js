6146e635c45d2f6a6e9f4bac1ec235c09e845af0
Arli <arli.machado@gmail.com>
2019-10-18 17:55:50 -0300

    Definindo tela de cadastro de usuario

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {}
+
   return (
     <Background>
-      <Text>Cadastro</Text>
+      <Container>
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Nome completo"
+            value={name}
+            onChangeText={setName}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitaliza="none"
+            placeholder="Digite de email"
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            returnKeyType="send"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <SubmitButton onPress={handleSubmit}>Criar conta</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
