91d7391739d7e28ca24ba130d85f45531a8afe24
Arli Machado <arli.machado@gmail.com>
2019-10-21 20:44:20 -0300

    Definindo saga de autenticacao

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,13 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
+
+import { signIn, signUp, setToken } from './auth';
+import { Types as AuthTypes } from '../ducks/auth';
 
 export default function* rootSaga() {
-  return yield all([]);
+  const { SIGN_IN_REQUEST, SIGN_UP_REQUEST } = AuthTypes;
+  return yield all([
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest(SIGN_IN_REQUEST, signIn),
+    takeLatest(SIGN_UP_REQUEST, signUp),
+  ]);
 }
